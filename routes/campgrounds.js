cb91af89be83e313a56654798be1a369d49ac319
Maria Regina Sirilan <ahhreggi@hotmail.com>
2020-11-14 02:01:28 -0500

    Add routes to group similar paths

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,26 +4,39 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync'); // async wrapper utility
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const Campground = require('../models/campground');
 
-// INDEX route - READ and display all data from the database
-// (e.g., a page to display all campgrounds)
-// Execute while catching any errors, and if so, pass to next() (the basic error handler)
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    // INDEX route - READ and display all data from the database
+    // (e.g., a page to display all campgrounds)
+    // Execute while catching any errors, and if so, pass to next() (the basic error handler)
+    .get(catchAsync(campgrounds.index))
+    // NEW/CREATE route - CREATE new data and insert it into the database
+    // (e.g., a page with a form that submits new data)
+    // Require that the user is logged in (ideally, the user should never even make it to this form, but it's best to protect it anyways)
+    // Execute while validating data and catching any errors, and if so, pass to next() (the basic error handler)
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 // NEW/CREATE route - CREATE new data and insert it into the database
 // (e.g., a page with a form that submits new data)
 // NOTE: Order matters! This must precede the show route due to the url request
 // Require that the user is logged in
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
-// Require that the user is logged in (ideally, the user should never even make it to this form, but it's best to protect it anyways)
-// Execute while validating data and catching any errors, and if so, pass to next() (the basic error handler)
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-// SHOW route - READ and display specific data from the database
-// (e.g., a page for a specific campground)
-// Execute while catching any errors, and if so, pass to next() (the basic error handler)
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    // SHOW route - READ and display specific data from the database
+    // (e.g., a page for a specific campground)
+    // Execute while catching any errors, and if so, pass to next() (the basic error handler)
+    .get(catchAsync(campgrounds.showCampground))
+    // MIDDLEWARE: the edit form POST request is converted into PUT by app.use(methodOverride('_method'));
+    // Require that the user is logged in
+    // Check that the user is the author
+    // Execute while validating data and catching any errors, and if so, pass to next() (the basic error handler)
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    // DELETE route - DESTROY existing data within the database
+    // (e.g., a button on a campground page to delete it)
+    // Require that the user is logged in
+    // Check that the user is the author
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 // EDIT route - UPDATE existing data within the database
 // (e.g., a page for modifying a specific campground)
@@ -31,17 +44,5 @@ router.get('/:id', catchAsync(campgrounds.showCampground));
 // Check that the user is the author
 // Execute while catching any errors, and if so, pass to next() (the basic error handler)
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-// MIDDLEWARE: the edit form POST request is converted into PUT by app.use(methodOverride('_method'));
-// Require that the user is logged in
-// Check that the user is the author
-// Execute while validating data and catching any errors, and if so, pass to next() (the basic error handler)
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-// DELETE route - DESTROY existing data within the database
-// (e.g., a button on a campground page to delete it)
-// Require that the user is logged in
-// Check that the user is the author
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,22 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
 const users = require('../controllers/users');
 
-// NEW/CREATE route - CREATE new data and insert it into the database
-// (e.g., a user registration page)
-router.get('/register', users.renderRegister);
-// Execute while validating data and catching any errors, and if so, pass to next() (the basic error handler)
-router.post('/register', catchAsync(users.register));
+router.route('/register')
+    // NEW/CREATE route - CREATE new data and insert it into the database
+    // (e.g., a user registration page)
+    .get(users.renderRegister)
+    // Execute while validating data and catching any errors, and if so, pass to next() (the basic error handler)
+    .post(catchAsync(users.register))
 
-// SHOW route - READ and display specific data from the database
-// (e.g., a login page)
-// Prompt the user to log in
-router.get('/login', users.renderLogin);
-// Authenticate user information and log in or alert issues accordingly
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    // SHOW route - READ and display specific data from the database
+    // (e.g., a login page)
+    // Prompt the user to log in
+    .get(users.renderLogin)
+    // Authenticate user information and log in or alert issues accordingly
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout);
 
